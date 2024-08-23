200c6373a4dbe490a17710ae4dc081702924e88a
jeung1018 <mylife11018@gmail.com>
2024-08-23 12:58:54 -0700

    secret-test13

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import json
 import pandas as pd
 from PIL import Image, ImageOps, ImageDraw
 
+st.write("aws_region:", st.secrets["AWS_REGION"])
+
 # Streamlit page configuration
 st.set_page_config(page_title="Verdigris Chatbot", page_icon=":robot_face:", layout="wide")
 
