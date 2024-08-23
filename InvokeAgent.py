68248e7bccafc6b311e963ea76aef120d303a9a9
jeung1018 <mylife11018@gmail.com>
2024-08-23 12:37:46 -0700

    secret-test5

--- InvokeAgent.py
+++ InvokeAgent.py
@@ -21,7 +21,7 @@ from dotenv import load_dotenv
 # agentId = os.getenv("AGENT_ID")
 # agentAliasId = os.getenv("AGENT_ALIAS_ID")
 # theRegion = os.getenv("AWS_REGION")
-st.write("theRegion:", st.secrets[AWS_REGION])
+st.write("theRegion", st.secrets["AWS_REGION"])
 
 def extract_metadata(split_response):
     # Initialize an empty list to store dictionaries containing unique title, category, and URL
