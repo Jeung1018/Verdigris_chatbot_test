1e7307af509124d2b54bd53dc3b4d13a6dfd4550
jeung1018 <mylife11018@gmail.com>
2024-08-27 20:44:51 -0700

    design change2

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def clear_selectbox():
 # Title with logo
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("verdigris_logo.png", width=150)
+    st.image("verdigris_logo.png", width=120)
 with col2:
     st.title("Verdigris Chatbot")
     st.write("powered by Claude 3.5 Sonnet")
